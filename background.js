a3870c8c5b854ca24605d382441a1c4bf6b99f53
Falshtynskii Aleksei <aleksei.falsthynskii@gmail.com>
2020-11-06 01:12:41 +0300

    tabs

--- background.js
+++ background.js
@@ -1,46 +1,45 @@
 var RESOLUTION = 'RESOLUTION';
-var init = false;
-var resolutions;
-var m3u8Urls;
-var tsUrls;
-var contentData;
-var httpUrl;
-var downloadId;
-var index;
+var tabs = {};
+var tabId;
 
 chrome.downloads.setShelfEnabled(false);
 
-chrome.browserAction.disable();
+chrome.tabs.onActivated.addListener(function (activeInfo) {
+    this.tabId = activeInfo.tabId;
+    if (tab()) {
+        enablePopup();
+    } else {
+        disablePopup();
+    }
+});
 
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
     if (message.msgId == 'showPopup') {
-        sendResponse(resolutions);
+        sendResponse({ contentData: tab().contentData, resolutions: tab().resolutions });
     } else if (message.msgId == 'clickDownload') {
-        sendResponse(message);
-        var m3u8Url = m3u8Urls[message.buttonId];
+        tab().buttonId = message.buttonId;
+        var m3u8Url = tab().m3u8Urls[tab().buttonId];
         fetch(m3u8Url).then(r => r.text()).then(r => {
-            tsUrls = r.split('\n').filter(s => !s.startsWith('#'));
-            httpUrl = m3u8Url.substr(0, m3u8Url.lastIndexOf('/') + 1);
-            index = 0;
-            downloadPart(index);
+            tab().tsUrls = r.split('\n').filter(s => !s.startsWith('#'));
+            tab().httpUrl = m3u8Url.substr(0, m3u8Url.lastIndexOf('/') + 1);
+            tab().index = 0;
+            sendResponse({ buttonId: message.buttonId, parts: tab().tsUrls.length, part: 0 });
+            downloadPart(tab().index);
         });
     }
 });
 
 chrome.webRequest.onBeforeRequest.addListener(
     function (response) {
-        if (!init && !response.url.includes('index')) {
-            init = true;
+        if ((!tab() || !tab().init) && !response.url.includes('index')) {
+            if (!tab()) {
+                initTab();
+            }
+            tab().init = true;
             fetch(response.url).then(r => r.text()).then(r => {
-                resolutions = r.split('\n').slice(1).filter(s => s.startsWith('#')).map(s => s.substr(s.indexOf(RESOLUTION) + RESOLUTION.length + 1));
-                m3u8Urls = r.split('\n').filter(s => !s.startsWith('#'));
-                chrome.browserAction.enable();
-                
-                chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-                    chrome.tabs.sendMessage(tabs[0].id, { msgId: "enablePopup" }, function (response) {
-                        contentData = response;
-                    });
-                });
+                tab().resolutions = r.split('\n').slice(1).filter(s => s.startsWith('#')).map(s => s.substr(s.indexOf(RESOLUTION) + RESOLUTION.length + 1));
+                tab().m3u8Urls = r.split('\n').filter(s => !s.startsWith('#'));
+                enablePopup();
             });
         }
         return response;
@@ -50,24 +49,46 @@ chrome.webRequest.onBeforeRequest.addListener(
 );
 
 chrome.downloads.onChanged.addListener(function ({ id, state }) {
-    if (id == downloadId && state && state.current == 'complete') {
-        console.log('complete ' + tsUrls[index]);
-        eraseDownload(index);
-        downloadPart(++index);
+    if (id == tab().downloadId && state && state.current == 'complete') {
+        console.log('complete ' + tab().tsUrls[tab().index]);
+        chrome.runtime.sendMessage({ msgId: 'completePart', parts: tab().tsUrls.length, part: tab().index + 1});
+        eraseDownload(tab().index);
+        downloadPart(++tab().index);
     }
 });
 
+function enablePopup() {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, { msgId: "enablePopup" }, function (contentData) {
+            tab().contentData = contentData;
+            chrome.browserAction.enable();
+        });
+    });
+}
+
+function disablePopup() {
+    chrome.browserAction.disable();
+}
+
 function downloadPart(i) {
-    if (i == tsUrls.length - 1) {
+    if (i == tab().tsUrls.length - 1) {
         console.log('COMPLETE!!!');
     } else {
-        console.log('downloading ' + tsUrls[i]);
+        console.log('downloading ' + tab().tsUrls[i]);
         var url = getUrl(i);
-        var filename = `${clearName(contentData.name)}/${clearName(contentData.season)}/${clearName(contentData.episode)}/${tsUrls[i]}`;
+        var filename = `${clearName(tab().contentData.name)}/${clearName(tab().contentData.season)}/${clearName(tab().contentData.episode)}/${tab().resolutions[tab().buttonId]}/${tab().tsUrls[i]}`;
         chrome.downloads.download({ url: url, filename: filename }, function (id) {
-            downloadId = id;
+            tab().downloadId = id;
         });
     }
+}
+
+function tab() {
+    return this.tabs[this.tabId];
+}
+
+function initTab() {
+    this.tabs[this.tabId] = {};
 }
 
 function clearName(s) {
@@ -75,11 +96,11 @@ function clearName(s) {
 }
 
 function eraseDownload(i) {
-    chrome.downloads.erase({ query: [tsUrls[i]] }, function (eraseIds) {
+    chrome.downloads.erase({ query: [tab().tsUrls[i]] }, function (eraseIds) {
         console.log(JSON.stringify(eraseIds));
     });
 }
 
 function getUrl(i) {
-    return httpUrl + tsUrls[i];
+    return tab().httpUrl + tab().tsUrls[i];
 }
--- popup.js
+++ popup.js
@@ -1,16 +1,32 @@
-chrome.runtime.sendMessage({ msgId: 'showPopup' }, function (resolutions) {
-    var body = document.getElementsByTagName('body')[0];
+chrome.runtime.sendMessage({ msgId: 'showPopup' }, function (response) {
+    var contentData = response.contentData;
+    var resolutions = response.resolutions;
+
+    var divTitle = document.getElementById('title');
+    divTitle.innerHTML = `${contentData.name}. ${contentData.season}. ${contentData.episode}`;
+    var divButtons = document.getElementById('buttons');
 
     resolutions.forEach((resolution, index) => {
-        body.innerHTML += '<button id="' + index + '">Download ' + resolution + '</button>';
+        divButtons.innerHTML += `<div id='div${index}'></div><button id='${index}'>Download '${resolution}'</button></div>`;
     });
 
     Array.from(document.getElementsByTagName('button')).forEach(btn => {
         btn.onclick = function () {
             chrome.runtime.sendMessage({ msgId: 'clickDownload', buttonId: this.id }, function (response) {
                 chrome.extension.getBackgroundPage().console.log('clicked button ' + response.buttonId);
-                document.getElementById(response.buttonId).innerHTML = '<span>Downloading</span>'
+                refreshParts(response.buttonId, response.parts, response.part);
             });
         };
     });
 });
+
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+    if (message.msgId == 'completePart') {
+        refreshParts(message.buttonId, message.parts, message.part);
+    }
+});
+
+function refreshParts(buttonId, parts, part) {
+    var div = document.getElementById(`div${buttonId}`);
+    div.innerHTML = `${part} / ${parts}`;
+}
